1285f57abb10e9ab547f32e61cbe7b848cba0604
fridaynight920 <104913092+fridaynight920@users.noreply.github.com>
2023-09-06 17:57:48 +0800

    把HJ分支的文件迁过来了

--- Assets/HJ/Script/move.cs
+++ Assets/HJ/Script/move.cs
@@ -2,42 +2,48 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class move : MonoBehaviour
 {
-   
+    private float[] frequency = new float[50]; 
+    private float[] amplitude = new float[50]; 
+    private float[] speed = new float[50]; 
+ // 随机选择的数值
+    
     Vector3 CenterPosition = Vector3.zero; //圆心的位置，我设定在（0，0）点
     Vector3 r; //圆半径，也就是要旋转的向量。
     private GameObject[] tar;
     private Transform m_transform;
     
-    
-    
-    
-
+   
+   // public float frequency = 1f; // 频率
+  
     public void Start()
     {
+        GenerateRandomValues();  // 初始化随机数数组
         
-       // tar = GameObject.FindGameObjectsWithTag("input");
-       
-
     }
-   
+
     public void Update()
     {
-        tar = DynamicPrefabLoader.Target;
-        int sum = tar.Length;
-        Debug.Log(sum);
 
+      
+        
+      tar = DynamicPrefabLoader.Target;
+       
         for (int i = 0; i < tar.Length; i++)
         {
             m_transform = GameObject.FindGameObjectsWithTag("input")[i].GetComponent<Transform>();
-            //Debug.Log(i+1);
-            //加入列表，打印序列号
             r = m_transform.position - CenterPosition;
-            r = Quaternion.AngleAxis(10 * Time.deltaTime, Vector3.up) * r;
+            r = Quaternion.AngleAxis(speed[i] * Time.deltaTime, Vector3.up) * r;
             m_transform.position = CenterPosition + r;
 
+            float newY = Mathf.Sin(Time.time * frequency[i]) * amplitude[i];
+            m_transform.position = new Vector3(m_transform.position.x, newY, m_transform.position.z);
+           
+           Debug.Log(frequency[i]);
+           Debug.Log(amplitude[i]);
             //圆周运动
             // //以每秒180度的速度旋转“半径”向量，因为我做2D游戏，所以旋转轴是Z轴。
             // //圆心位置 + 半径 = 圆上的点
@@ -46,7 +52,18 @@ public class move : MonoBehaviour
         }
 
     }
+    void GenerateRandomValues()
+    {
+        for (int i = 0; i < 50;  i++)
+        {
+            frequency[i] = Random.Range(0.5f, 2f);
+            amplitude[i] = Random.Range(0.7f, 3f);
+            speed[i] = Random.Range(15f, 40f);
+            
+         //   Debug.Log("Random value for i = " + i + ": " + randomValues[i]);
+          
+        }
+    }    
     
-    
-    
+   
 }
